9c6ace3fb98393f81c7c015ac20c351a4fdf27ee
RoshanrksKrSingh <roshankrsingh94@gmail.com>
2025-06-20 14:12:05 +0530

    app.js fill changed

--- app.js
+++ app.js
@@ -11,17 +11,21 @@ const app = express();
 
 const allowedOrigins = ['http://localhost:3000', 'http://127.0.0.1:5500'];
 
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   },
+//   credentials: true,               
+//   methods: ['GET', 'POST', 'PUT', 'DELETE']
+// }));
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true,               
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
+  origin: true,             //  Automatically reflect the origin from the request
+  credentials: true         // Allow cookies
 }));
 
 // JSON body parser middleware
